155022f638c156126637b4b04c8495d00ccbd26b
Will <38348991+willfretwell@users.noreply.github.com>
2018-04-16 11:04:27 +0100

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,6 @@ const webpack = require('webpack');
 const path = require('path');
 const pak = require('./package.json');
 
-const nodeEnv = process.env.NODE_ENV || 'development';
-
 const webpackConfig = {
   context: __dirname,
   entry: {
@@ -32,42 +30,6 @@ const webpackConfig = {
       },
     ],
   },
-  plugins: [
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify(nodeEnv),
-    }),
-  ],
 };
-
-if (nodeEnv === 'development') {
-  webpackConfig.devtool = 'source-map';
-  webpackConfig.devServer = { contentBase: './demo' };
-  webpackConfig.entry['heatmap-calendar-react'].unshift('webpack-dev-server/client?http://0.0.0.0:8080/');
-  webpackConfig.output.publicPath = '/';
-}
-
-if (nodeEnv === 'development' || nodeEnv === 'demo') {
-  webpackConfig.plugins.push(new webpack.DefinePlugin({
-    COMPONENT_NAME: JSON.stringify(pak.name),
-    COMPONENT_VERSION: JSON.stringify(pak.version),
-    COMPONENT_DESCRIPTION: JSON.stringify(pak.description),
-  }));
-}
-
-if (nodeEnv === 'production') {
-  webpackConfig.externals = {
-    react: {
-      root: 'React',
-      commonjs2: 'react',
-      commonjs: 'react',
-      amd: 'react',
-    },
-  };
-  webpackConfig.output.path = path.resolve(__dirname, 'build');
-  webpackConfig.plugins.push(new webpack.optimize.UglifyJsPlugin({
-    compress: { warnings: false },
-    sourceMap: false,
-  }));
-}
 
 module.exports = webpackConfig;
