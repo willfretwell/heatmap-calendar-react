cfee6134351fedc1df52baaf71145749adce28b6
Will <38348991+willfretwell@users.noreply.github.com>
2018-04-16 12:42:10 +0100

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -22,12 +22,26 @@ const webpackConfig = {
   module: {
     rules: [
       {
-        test: /\.jsx?$/,
-        exclude: /(node_modules)/,
-        use: [{
+        test: /\.js$/,
+        include: path.resolve(__dirname, 'src'),
+        exclude: /(node_modules|bower_components|build)/,
+        use: {
           loader: 'babel-loader',
-        }],
+          options: {
+            presets: ['env']
+          }
+        }
       },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+            'css-loader'
+        ]
+     },
+        {
+            test: /\.(jpe?g|png|gif|svg)$/i, loader: "url-loader?name=src/images/[name].[ext]"
+        } 
     ],
   },
 };
