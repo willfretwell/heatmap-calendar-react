ecc2f011db3cdf5aaf2062904759a078370eb9fd
Will <38348991+willfretwell@users.noreply.github.com>
2018-04-16 13:48:41 +0100

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,49 @@
-module.exports = {
-  entry: [
-    './src/index.jsx'
-  ],
+const webpack = require('webpack');
+const path = require('path');
+const pak = require('./package.json');
+
+const webpackConfig = {
+  context: __dirname,
+  entry: {
+    'heatmap-calendar-react': [
+      path.resolve(__dirname, 'src', 'index.jsx'),
+    ],
+  },
+  output: {
+    path: path.resolve(__dirname),
+    filename: 'index.js',
+    library: 'HeatMapGraph',
+    libraryTarget: 'umd',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+    modules: ['node_modules'],
+  },
   module: {
     rules: [
       {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: ['babel-loader']
-      }
-    ]
+        test: /\.js$/,
+        include: path.resolve(__dirname, 'src'),
+        exclude: /(node_modules|bower_components|build)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env']
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+            'css-loader'
+        ]
+     },
+        {
+            test: /\.(jpe?g|png|gif|svg)$/i, loader: "url-loader?name=src/images/[name].[ext]"
+        } 
+    ],
   },
-  resolve: {
-    extensions: ['*', '.js', '.jsx']
-  },
-  output: {
-    path: __dirname + '/dist',
-    publicPath: '/',
-    filename: 'bundle.js'
-  },
-  devServer: {
-    contentBase: './dist'
-  }
 };
+
+module.exports = webpackConfig;
