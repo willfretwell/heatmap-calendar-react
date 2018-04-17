549d1039a8cbe6e26fd4df969b0db12f49f725fb
Will Fretwell <github@williamfretwell.co.uk>
2018-04-17 11:12:26 +0100

    Trying to get this to work

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,52 @@
-var path = require('path');
+const webpack = require('webpack');
+const pkg = require('./package.json');
+const path = require('path');
+
+const libraryName = pkg.name;
+
 module.exports = {
-  entry: './index.js',
-  output: {
-    path: path.resolve(__dirname, 'build'),
-    filename: 'index.js',
-    libraryTarget: 'commonjs2'
-  },
+  entry: [
+    './index.js'
+  ],
   module: {
     rules: [
       {
-        test: /\.js$/,
-        include: path.resolve(__dirname),
-        exclude: /(node_modules|bower_components|build)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['env']
-          }
-        }
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
       }
     ]
   },
+  resolve: {
+    extensions: ['*', '.js', '.jsx'],
+    alias: {
+      'react': path.resolve(__dirname, './node_modules/react'),
+      'react-dom': path.resolve(__dirname, './node_modules/react-dom'),
+    }
+  },
   externals: {
-    'react': 'commonjs react'
+    react: {
+        commonjs: "react",
+        commonjs2: "react",
+        amd: "React",
+        root: "React"
+    },
+    "react-dom": {
+        commonjs: "react-dom",
+        commonjs2: "react-dom",
+        amd: "ReactDOM",
+        root: "ReactDOM"
+    }
+  },
+  output: {
+    path: path.join(__dirname, './dist'),
+    filename: 'bundle.js',
+    library: libraryName,
+    libraryTarget: 'umd',
+    publicPath: '/dist/',
+    umdNamedDefine: true
+  },
+  devServer: {
+    contentBase: './dist'
   }
 };
